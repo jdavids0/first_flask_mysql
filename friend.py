8e4578998d1574014bb675b35b2d34f6e08a6943
jdavids0 <jbrdavidson@gmail.com>
2022-04-13 12:28:37 -0600

    edits after lecture

--- friend.py
+++ friend.py
@@ -2,7 +2,7 @@
 # need to call connectToMySQL function EVERY TIME want to execute query bc connection closes as soon as query finishes executing
 from mysqlconnection import connectToMySQL
 # model the class after the friend table from our database
-class Friend:
+class Friend():
     def __init__( self , data ):
         self.id = data['id']
         self.first_name = data['first_name']
@@ -12,7 +12,7 @@ class Friend:
         self.updated_at = data['updated_at']
     # Now we use class methods to query our database
     @classmethod
-    def get_all(cls):
+    def get_all_friends(cls):
         query = "SELECT * FROM friends;"
         # make sure to call the connectToMySQL function with the schema you are targeting.
         results = connectToMySQL('first_flask').query_db(query)
@@ -22,4 +22,28 @@ class Friend:
         for friend in results:
             friends.append( cls(friend) )
         return friends
+
+    @classmethod
+    def create_friend(cls, data ):
+        query = "INSERT INTO friends ( first_name , last_name , occupation , created_at, updated_at ) VALUES ( %(fname)s , %(lname)s , %(occ)s , NOW() , NOW() );"
+        # data is a dictionary that will be passed into the save method from server.py
+        result = connectToMySQL('first_flask').query_db( query, data )
+
+        return result
+
+        
+# query = "UPDATE friends SET first_name=%(fn)s WHERE id=%(id_num)s;"
+# data = {
+#   "fn": # possibly a value from form,
+#   "id_num": # possibly a value from the url,
+# }
+# mysql.query_db(query, data)
+# 
+# i.e.
+# 
+# query = "SELECT * FROM users WHERE email = %(email)s;"
+# data = { 'email' : request.form['email'] }
+# result = mysql.query_db(query, data)
+
+
             
--- server.py
+++ server.py
@@ -1,13 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 # import the class from friend.py
 from friend import Friend
 app = Flask(__name__)
 @app.route("/")
 def index():
     # call the get all classmethod to get all friends
-    friends = Friend.get_all()
-    print(friends)
-    return render_template("index.html")
+    friends = Friend.get_all_friends()
+    # print(friends)
+    return render_template("index.html", friends=friends)
+
+from friend import Friend
+@app.route('/friend/new', methods=["POST"])
+def new_friend():
+    # First we make a data dictionary from our request.form coming from our template.
+    # The keys in data need to line up exactly with the variables in our query string.
+    data = {
+        "fname": request.form["fname"],
+        "lname" : request.form["lname"],
+        "occ" : request.form["occ"]
+    }
+    # We pass the data dictionary into the save method from the Friend class.
+    Friend.save(data)
+    # Don't forget to redirect after saving to the database.
+    return redirect('/')
+
+
             
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
